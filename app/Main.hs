3861eb6f281ed6182602aee402f77e1237623a8d
iCaran <pratapmorgan@gmail.com>
2024-05-25 04:50:10 +0530

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -30,15 +30,13 @@ import Network.Socket as NetSock
 
       AddrInfo(addrAddress, addrFamily),
 
-    --   SocketType(Stream), setSocketOption, SocketOption (ReuseAddr) )
-
       SocketType(Stream), setSocketOption, SocketOption (ReuseAddr), Socket )
 
 import Network.Socket.ByteString as NBS (send, recv)
 
--- import System.IO (BufferMode (..), hSetBuffering, stdout)
+-- import System.IO (BufferMode (..), hSetBuffering, stdout, openFile, IOMode (ReadMode), hGetContents)
 
-import System.IO (BufferMode (..), hSetBuffering, stdout, openFile, IOMode (ReadMode), hGetContents)
+import System.IO (BufferMode (..), hSetBuffering, stdout, openFile, IOMode (..), hGetContents, hClose)
 
 import qualified System.IO as SIO
 
@@ -50,11 +48,13 @@ import Control.Concurrent (threadDelay, forkIO)
 
 import qualified Data.List as List
 
--- import Control.Exception (catch, AsyncException (UserInterrupt))
-
 import Control.Exception (catch, AsyncException (UserInterrupt), SomeException (SomeException))
 
 import System.Environment as SE (getArgs)
+
+import Data.ByteString (hPut)
+
+newtype HttpError = HttpError String deriving (Show)
 
 createBody :: [[Char]] -> [Char]
 
@@ -70,15 +70,33 @@ createResponse request headers body =
 
     ("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " ++ show (BC.length body) ++ "\r\n\r\n" ++ BC.unpack body)
 
+data Method =
+
+    GET
+
+    | POST
+
+    deriving (Show)
+
+parseMethod "GET" = GET
+
+parseMethod "POST" = POST
+
 data HttpMsg = HttpMsg {
 
-    method :: BC.ByteString
+    -- method :: BC.ByteString
+
+    method :: Method
 
     , path :: BC.ByteString
 
     , protocol :: [BC.ByteString]
 
-    , headers :: [(BC.ByteString, BC.ByteString)]} deriving (Show)
+    -- , headers :: [(BC.ByteString, BC.ByteString)]} deriving (Show)
+
+    , headers :: [(BC.ByteString, BC.ByteString)]
+
+    , body :: BC.ByteString} deriving (Show)
 
 destructHeader :: [BC.ByteString] -> Maybe (BC.ByteString, BC.ByteString)
 
@@ -98,27 +116,55 @@ parseHeaders = foldr ((\header acc ->
 
         ) . destructHeader . BC.words) []
 
-parseMsg :: BC.ByteString -> HttpMsg
+-- parseMsg :: BC.ByteString -> HttpMsg
+
+parseMsg :: BC.ByteString -> Either HttpMsg HttpError
 
 parseMsg msg =
 
-    let first:headers = map (BC.dropEnd 1) $ BC.lines msg in
+    -- let first:headers = map (BC.dropEnd 1) $ BC.lines msg in
 
-        let method:path:protocol = BC.split ' ' first in
+    --     let method:path:protocol = BC.split ' ' first in
 
-            HttpMsg {method = method, path = path, protocol = protocol, headers = parseHeaders headers}
+    --         HttpMsg {method = method, path = path, protocol = protocol, headers = parseHeaders headers}
+
+    
+
+    let msgLines = BC.lines msg in
+
+        case List.elemIndex (BC.pack "\r") msgLines of
+
+            Just bodyStartIdx ->
+
+                let (meta, body) = List.splitAt bodyStartIdx msgLines
+
+                    first:headers = map (BC.dropEnd 1) meta
+
+                    method:path:protocol = BC.split ' ' first in
+
+                        Left HttpMsg {method = parseMethod . BC.unpack $ method, path = path, protocol = protocol, headers = parseHeaders headers, body = body & List.dropWhile (== BC.pack ['\r']) & BC.unlines & BC.dropEnd 1}
+
+            Nothing ->
+
+                Right $ HttpError "Malformed http message"
 
 test :: BC.ByteString
 
-test = BC.pack "GET /user-agent HTTP/1.1\r\n\
+-- test = BC.pack "GET /user-agent HTTP/1.1\r\n\
 
-\Host: localhost:4221\r\n\ 
+-- \Host: localhost:4221\r\n\ 
 
-\User-Agent: curl/7.64.1\r\n\r\n"
+-- \User-Agent: curl/7.64.1\r\n\r\n"
+
+test = BC.pack "POST /files/readme.txt HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/8.4.0\r\nAccept: */*\r\nContent-Length: 11\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nhello world"
+
+-- "GET /user-agent HTTP/1.1\r\n\
+
+-- \Host: localhost:4221\r\n\ 
+
+-- \User-Agent: curl/7.64.1\r\n\r\n"
 
 handleUserAgent request =
-
-    -- case List.find (\(k,_) -> k == BC.pack ("User-Agent")) (headers request) of
 
     case List.find (\(k,_) -> k == BC.pack "User-Agent") (headers request) of
 
@@ -148,11 +194,17 @@ handleGetFile sock msg (Just dir) file = do
 
         send sock $ BC.pack ("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " ++ show (length contents) ++ "\r\n\r\n" ++ contents))
 
-        (\(SomeException _) -> 
+        -- (\(SomeException _) -> 
+
+        --     send sock "HTTP/1.1 404 NOT FOUND\r\n\r\n")
+
+        (\(SomeException _) ->
 
             send sock "HTTP/1.1 404 Not Found\r\n\r\n")
 
 handleGetFile sock _ Nothing _ = do
+
+    -- send sock "HTTP/1.1 404 NOT FOUND\r\n\r\n"
 
     send sock "HTTP/1.1 404 Not Found\r\n\r\n"
 
@@ -214,23 +266,13 @@ main = do
 
                 -- Handle the clientSocket as needed...
 
-                -- buff <- recv clientSocket 91
-
                 buff <- recv clientSocket 1024
 
-                BC.putStrLn $ "Received " <> BC.pack (show buff)
+                -- BC.putStrLn $ "Received " <> BC.pack (show buff)
 
-                let msg = parseMsg buff
+                putStrLn $ "Received " <> show buff
 
-                -- let resp = case map BC.unpack . BC.split '/' $ msg.path of
-
-                --             ("":"":_) -> "HTTP/1.1 200 OK\r\n\r\n"
-
-                --             ("":"echo":payload) -> createBody payload
-
-                --             ("":"user-agent":_) -> handleUserAgent msg
-
-                --             _ -> "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+                -- let msg = parseMsg buff
 
                 -- let resp = case map BC.unpack . BC.split '/' $ msg.path of
 
@@ -244,17 +286,37 @@ main = do
 
                 --             _ -> "HTTP/1.1 404 NOT FOUND\r\n\r\n"
 
-                case map BC.unpack . BC.split '/' $ msg.path of
+                -- case map BC.unpack . BC.split '/' $ msg.path of
 
-                            ("":"":_) -> send clientSocket "HTTP/1.1 200 OK\r\n\r\n"
+                --             ("":"":_) -> send clientSocket "HTTP/1.1 200 OK\r\n\r\n"
 
-                            ("":"echo":payload) -> send clientSocket $ BC.pack $ createBody payload
+                --             ("":"echo":payload) -> send clientSocket $ BC.pack $ createBody payload
 
-                            ("":"user-agent":_) -> send clientSocket $ BC.pack $ handleUserAgent msg
+                --             ("":"user-agent":_) -> send clientSocket $ BC.pack $ handleUserAgent msg
 
-                            ("":"files":filename:_) -> handleGetFile clientSocket msg directory filename
+                --             ("":"files":filename:_) -> handleGetFile clientSocket msg directory filename
 
-                            _ -> send clientSocket "HTTP/1.1 404 Not Found\r\n\r\n"
+                --             _ -> send clientSocket "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+
+                let Left msg = parseMsg buff
+
+                let pathParts = map BC.unpack $ BC.split '/' msg.path
+
+                putStrLn $ "Path " <> show pathParts
+
+                case (msg.method, pathParts) of
+
+                            (GET, "":"":_ )-> send clientSocket "HTTP/1.1 200 OK\r\n\r\n"
+
+                            (GET, "":"echo":payload) -> send clientSocket $ BC.pack $ createBody payload
+
+                            (GET, "":"user-agent":_) -> send clientSocket $ BC.pack $ handleUserAgent msg
+
+                            (GET, "":"files":filename:_) -> handleGetFile clientSocket msg directory filename
+
+                            (POST, "":"files":filename:_) -> handlePostFile clientSocket msg directory filename
+
+                            (_, _) -> send clientSocket "HTTP/1.1 404 Not Found\r\n\r\n"
 
                     -- if path == "/" then
 
@@ -266,17 +328,11 @@ main = do
 
                 -- _ <- send clientSocket (BC.pack resp)
 
-                -- BC.putStrLn $ "Resp is " <> BC.pack (show resp)
-
-                -- _ <- send clientSocket (BC.pack resp)
-
                 close clientSocket
 
         )
 
         (   \UserInterrupt ->
-
-            -- do 
 
             do
 
@@ -285,3 +341,20 @@ main = do
                 BC.putStrLn "Interrupt exception - closing server"
 
         )
+
+handlePostFile :: Socket -> HttpMsg -> Maybe [Char] -> [Char] -> IO Int
+
+handlePostFile sock msg dir file = do
+
+    putStrLn $ "Posted file " <> file
+
+    let Just d = dir
+
+    handle <- openFile (concat [d, "/", file]) WriteMode
+
+    BC.hPut handle msg.body
+
+    hClose handle
+
+    send sock "HTTP/1.1 201 Created\r\n\r\n"
+
